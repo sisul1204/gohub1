3d9de4be0bd9f0f74068609e8e89415a6579c04e
lizhipeng1021 <lizhipeng1021@163.com>
2023-12-26 17:52:30 +0800

    panic recovery

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -16,7 +16,7 @@ func SetupRoute(route *gin.Engine) {
 }
 
 func registerGlobalMiddleWare(router *gin.Engine) {
-	router.Use(middlewares.Logger(), gin.Recovery())
+	router.Use(middlewares.Logger(), middlewares.Recovery())
 }
 
 func setup404Handler(router *gin.Engine) {
